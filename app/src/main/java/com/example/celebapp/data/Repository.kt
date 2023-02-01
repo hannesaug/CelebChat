db7d0136a4a108c41ae1de6bc18bcea6c28c361b
hannesaugustus <hannesaugustus29@gmail.com>
2023-02-01 17:11:05 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/celebapp/data/Repository.kt
@@ -0,0 +1,6 @@
+package com.example.celebapp.data
+
+class Repository  {
+
+
+}
---
+++ app/src/main/java/com/example/celebapp/data/model/Contact.kt
@@ -0,0 +1,6 @@
+package com.example.celebapp.data.model
+
+import android.media.Image
+
+data class Contact( val name: String, val image: Int, val chat: MutableList<String>)  {
+}
